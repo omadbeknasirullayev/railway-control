640d10acce7c8f2013182832df466c38bcd69de2
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2025-12-11 06:02:10 +0500

    add authorization station module

--- src/api/station/dto/update-station.dto.ts
+++ src/api/station/dto/update-station.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateStationDto } from './create-station.dto';
+import { ApiPropertyOptional, PartialType } from "@nestjs/swagger";
+import { CreateStationDto } from "./create-station.dto";
+import { IsBoolean, IsOptional } from "class-validator";
 
-export class UpdateStationDto extends PartialType(CreateStationDto) {}
+export class UpdateStationDto extends PartialType(CreateStationDto) {
+	@ApiPropertyOptional()
+	@IsOptional()
+	@IsBoolean()
+	public isActive?: boolean;
+}
--- src/api/station/station.controller.ts
+++ src/api/station/station.controller.ts
@@ -8,13 +8,22 @@ import {
 	Query,
 	ParseIntPipe,
 	Patch,
+	UseGuards,
 } from "@nestjs/common";
 import { StationService } from "./station.service";
 import { CreateStationDto } from "./dto/create-station.dto";
 import { FilterDto } from "src/common/dto/filter.dto";
 import { ILike } from "typeorm";
 import { UpdateStationDto } from "./dto/update-station.dto";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { JwtAuthGuard } from "../auth/user/AuthGuard";
+import { RolesGuard } from "../auth/roles/RoleGuard";
+import { RolesDecorator } from "../auth/roles/RolesDecorator";
+import { RolesEnum } from "src/common/database/Enums";
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@RolesDecorator(RolesEnum.SUPER_ADMIN, RolesEnum.ADMIN)
+@ApiBearerAuth()
 @Controller("station")
 export class StationController {
 	constructor(private readonly stationService: StationService) {}
@@ -29,7 +38,7 @@ export class StationController {
 		return this.stationService.findAllWithPagination({
 			skip: query.page,
 			take: query.page_size,
-			where: { name: ILike(`%${query.search || ""}%`), isActive: true },
+			where: { name: ILike(`%${query.search || ""}%`), isDeleted: false },
 		});
 	}
 
@@ -39,8 +48,8 @@ export class StationController {
 	}
 
 	@Patch(":id")
-	update(@Param("id", ParseIntPipe) id: number, @Body() updateStationDto: UpdateStationDto) {
-		return this.stationService.update(id, updateStationDto);
+	update(@Param("id", ParseIntPipe) id: number, @Body() dto: UpdateStationDto) {
+		return this.stationService.update(id, dto);
 	}
 
 	@Delete(":id")
